442dacf1bafa67afef9ce23653211281a5121231
marcelacrvalho <marcelacrvalho@icloud.com>
2022-10-13 18:31:14 -0300

    Altera nome do pacote get_short_usecase

--- lib/domain/usecases/get_short_url/get_short_url_usecase_imp.dart
+++ lib/domain/usecases/get_short_url/get_short_url_usecase_imp.dart
@@ -3,7 +3,8 @@ import '../../domain.dart';
 class GetShortUrlUsecaseImplement implements GetShortUrlUsecase {
   @override
   ShortUrlEntity call(String extensiveUrl) {
-    return extensiveUrl.isNotEmpty ? ShortUrlEntity(resultUrl: 'shorturl.com') : ShortUrlEntity(resultUrl: 'Erro');
+    return extensiveUrl.isNotEmpty
+        ? ShortUrlEntity(resultUrl: 'shorturl.com')
+        : ShortUrlEntity(resultUrl: 'Erro');
   }
-
 }
